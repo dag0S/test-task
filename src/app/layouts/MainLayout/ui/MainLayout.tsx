771d23645d06ad019afdfa96b4f1c960b32ec506
Gosudarev Danila <danidagosudarev@gmail.com>
2025-11-04 15:40:51 +0300

    fix bug with metadata

--- src/app/layouts/MainLayout/ui/MainLayout.tsx
+++ src/app/layouts/MainLayout/ui/MainLayout.tsx
@@ -19,7 +19,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   title: "Alfa-Bank Test Task",
-  description: "Test assignment from Alfa-Bank",
+  description: "Test assignment from Alfa",
 };
 
 const MainLayout = ({
